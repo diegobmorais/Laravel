ef5a14cd3d48622441e8bb01a78828ff2d023837
Diegobmorais <diegobatistademorais@gmail.com>
2023-11-15 11:06:34 -0300

    update: melhorado paginas views do projeto (contato, principal e sobrenos), adicionado novos estilos e propriedades helper/assest nos arquivos de img

--- app-super-gestao/routes/web.php
+++ app-super-gestao/routes/web.php
@@ -3,25 +3,25 @@
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/','App\Http\Controllers\PrincipalController@principal');
+Route::get('/','App\Http\Controllers\PrincipalController@principal')->name('site.index');
 
-Route::get('/sobre-nos', 'App\Http\Controllers\SobreNosController@sobreNos');
+Route::get('/sobre-nos', 'App\Http\Controllers\SobreNosController@sobreNos')->name('site.sobrenos');
 
-Route::get('/contato', 'App\Http\Controllers\ContatoController@contato');
+Route::get('/contato', 'App\Http\Controllers\ContatoController@contato')->name('site.contato');
 Route::get('/login', function(){
     return 'login';
-});
+})->name('site.login');
 
 // Agrupamento de rotas para /app
 Route::prefix('/app')->group(function(){
 
     Route::get('/clientes', function(){
         return 'clientes';
-    });
+    })->name('app.clientes');
     Route::get('/fornecedores','App\Http\Controllers\FornecedorController@index' )->name('app.fornecedor');
     Route::get('/produtos', function(){
         return 'produtos';
-    });
+    })->name('app.produtos');
 
 });
 
