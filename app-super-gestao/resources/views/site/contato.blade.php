ccd82c03414c673b2b5673a9fbf8a1e90c37bd8c
Diego B Morais <128264029+diegobmorais@users.noreply.github.com>
2023-11-21 16:45:37 -0300

    update: adicionado regras de validação para o formulario na rota contato

--- app-super-gestao/resources/views/site/contato.blade.php
+++ app-super-gestao/resources/views/site/contato.blade.php
@@ -4,7 +4,7 @@
 <head>
     <title>Super Gestão - Contato</title>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="{{'css/style.basic.css'}}">
+    <link rel="stylesheet" href="{{ 'css/style.basic.css' }}">
 </head>
 
 <body>
@@ -27,16 +27,27 @@
         <div class="titulo-pagina">
             <h1>Entre em contato conosco</h1>
         </div>
-
+        @if ($errors->any())
+            <div style="width: 100%;  background:rgb(247, 167, 167); margin-top:15px">
+                @foreach ($errors->all() as $erro)
+                    {{ $erro }}
+                    <br>
+                @endforeach
+            </div>
+        @endif
         <div class="informacao-pagina">
             <div class="contato-principal">
-                <form action="{{ route('site.contato')}}" method="post">
+
+                <form action="{{ route('site.contato') }}" method="post">
                     @csrf
                     <input name="nome" type="text" placeholder="Nome" class="borda-preta">
+                    {{$errors->has('nome') ? $errors->first('nome') : ''}}
                     <br>
                     <input name="telefone" type="text" placeholder="Telefone" class="borda-preta">
+                    {{$errors->has('telefone') ? $errors->first('telefone') : ''}}
                     <br>
                     <input name="email" type="text" placeholder="E-mail" class="borda-preta">
+                    {{$errors->has('email') ? $errors->first('email') : ''}}
                     <br>
                     <select name="motivo_contato" class="borda-preta">
                         <option value="">Qual o motivo do contato?</option>
@@ -44,8 +55,10 @@
                         <option value="2">Elogio</option>
                         <option value="3">Reclamação</option>
                     </select>
+                    {{$errors->has('motivo_contato') ? $errors->first('motivo_contato') : ''}}
                     <br>
-                    <textarea name="mensagem" class="borda-preta">Preencha aqui a sua mensagem</textarea>
+                    <textarea name="mensagem" class="borda-preta" placeholder="Preencha aqui a sua mensagem"></textarea>
+                    {{$errors->has('mensagem') ? $errors->first('mensagem') : ''}}
                     <br>
                     <button type="submit" class="borda-preta">ENVIAR</button>
                 </form>
--- app-super-gestao/routes/web.php
+++ app-super-gestao/routes/web.php
@@ -8,7 +8,7 @@ Route::get('/','App\Http\Controllers\PrincipalController@principal')->name('site
 Route::get('/sobre-nos', 'App\Http\Controllers\SobreNosController@sobreNos')->name('site.sobrenos');
 
 Route::get('/contato', 'App\Http\Controllers\ContatoController@contato')->name('site.contato');
-Route::post('/contato', 'App\Http\Controllers\ContatoController@contato')->name('site.contato');
+Route::post('/contato', 'App\Http\Controllers\ContatoController@save')->name('site.contato');
 
 Route::get('/login', function(){
     return 'login';
@@ -20,6 +20,7 @@ Route::prefix('/app')->group(function(){
     Route::get('/clientes', function(){
         return 'clientes';
     })->name('app.clientes');
+    
     Route::get('/fornecedores','App\Http\Controllers\FornecedorController@index' )->name('app.fornecedor');
     Route::get('/produtos', function(){
         return 'produtos';
