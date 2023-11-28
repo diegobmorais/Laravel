c0dba05c92d1396ce09da48e5361876dad8acb42
Diego B Morais <128264029+diegobmorais@users.noreply.github.com>
2023-11-28 17:11:22 -0300

    update: adiciondo rota de middleware e superglobal session para validar acesso ao banco

--- app-super-gestao/routes/web.php
+++ app-super-gestao/routes/web.php
@@ -10,21 +10,20 @@ Route::get('/sobre-nos', 'App\Http\Controllers\SobreNosController@sobreNos')->na
 Route::get('/contato', 'App\Http\Controllers\ContatoController@contato')->name('site.contato');
 Route::post('/contato', 'App\Http\Controllers\ContatoController@save')->name('site.contato');
 
-Route::get('/login', function(){
-    return 'login';
-})->name('site.login');
+//rota para login no sistema
+Route::get('/login/{erro?}','App\Http\Controllers\LoginController@index')->name('site.login');
+Route::post('/login','App\Http\Controllers\LoginController@autenticar')->name('site.login');
 
 // Agrupamento de rotas para /app
-Route::prefix('/app')->group(function(){
+Route::middleware('autenticacao:padrao,visitante,p3,p4')->prefix('/app')->group(function() {
+    Route::get('/clientes', function(){return 'Clientes';})->name('app.clientes');
+    Route::get('/fornecedores', 'FornecedorController@index')->name('app.fornecedores');
+    Route::get('/produtos', function(){return 'produtos';})->name('app.produtos');
+});
 
-    Route::get('/clientes', function(){
-        return 'clientes';
-    })->name('app.clientes');
-    
-    Route::get('/fornecedores','App\Http\Controllers\FornecedorController@index' )->name('app.fornecedor');
-    Route::get('/produtos', function(){
-        return 'produtos';
-    })->name('app.produtos');
+Route::get('/teste/{p1}/{p2}', 'TesteController@teste')->name('site.teste');
 
+Route::fallback(function() {
+    echo 'A rota acessada não existe. <a href="'.route('site.index').'">clique aqui</a> para ir para página inicial';
 });
 
