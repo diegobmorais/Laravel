2d2699a31e1e20ecbe7c5022607e9727a66c9395
Diego B Morais <128264029+diegobmorais@users.noreply.github.com>
2023-11-28 21:33:23 -0300

    update: adicionado middleware de autenticação e configurações no arquivo kernel

--- app-super-gestao/routes/web.php
+++ app-super-gestao/routes/web.php
@@ -3,7 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/','App\Http\Controllers\PrincipalController@principal')->name('site.index');
+
+Route::get('/', 'App\Http\Controllers\PrincipalController@principal')->name('site.index');
+
 
 Route::get('/sobre-nos', 'App\Http\Controllers\SobreNosController@sobreNos')->name('site.sobrenos');
 
@@ -11,19 +13,18 @@ Route::get('/contato', 'App\Http\Controllers\ContatoController@contato')->name('
 Route::post('/contato', 'App\Http\Controllers\ContatoController@save')->name('site.contato');
 
 //rota para login no sistema
-Route::get('/login/{erro?}','App\Http\Controllers\LoginController@index')->name('site.login');
-Route::post('/login','App\Http\Controllers\LoginController@autenticar')->name('site.login');
+Route::get('/login/{erro?}', 'App\Http\Controllers\LoginController@index')->name('site.login');
+Route::post('/login', 'App\Http\Controllers\LoginController@autenticar')->name('site.login');
 
 // Agrupamento de rotas para /app
-Route::middleware('autenticacao:padrao,visitante,p3,p4')->prefix('/app')->group(function() {
-    Route::get('/clientes', function(){return 'Clientes';})->name('app.clientes');
+Route::middleware('autenticacao:padrao')->prefix('/app')->group(function () {
+    Route::get('/clientes', function () { return 'Clientes'; })->name('app.clientes');
     Route::get('/fornecedores', 'FornecedorController@index')->name('app.fornecedores');
-    Route::get('/produtos', function(){return 'produtos';})->name('app.produtos');
+    Route::get('/produtos', function () {return 'produtos'; })->name('app.produtos');
 });
 
 Route::get('/teste/{p1}/{p2}', 'TesteController@teste')->name('site.teste');
 
-Route::fallback(function() {
-    echo 'A rota acessada não existe. <a href="'.route('site.index').'">clique aqui</a> para ir para página inicial';
+Route::fallback(function () {
+    echo 'A rota acessada não existe. <a href="' . route('site.index') . '">clique aqui</a> para ir para página inicial';
 });
-
